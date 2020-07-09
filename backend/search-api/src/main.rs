c7ea5ef0190cf06d1eb5449a6f295c97f9d158de
Daniel Leite <1985.daniel@gmail.com>
2020-07-09 14:56:13 +0100

    first version complete. before refactoring

--- backend/search-api/src/main.rs
+++ backend/search-api/src/main.rs
@@ -1,12 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-
+use async_channel::SendError;
+use async_channel::{unbounded, Receiver, Sender};
+use futures::stream::Map;
 use isahc::prelude::*;
-use log::{info, trace};
-use rocket_contrib::json::Json;
+use log::{debug, info, trace};
+use oneshot::RecvError;
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
+use std::cmp::Reverse;
+use std::fmt::Display;
+use std::{convert::Infallible, error::Error};
+use warp::sse::ServerSentEvent;
+use warp::Buf;
+use warp::Filter;
+use warp::Stream;
 
 #[derive(Serialize)]
 struct SearchResult {
@@ -80,7 +88,7 @@ fn is_image(url: &str) -> Option<String> {
     {
         if url.ends_with(".gifv") {
             return Some(url.replace(".gifv", ".gif"));
-        } else if !url.ends_with(".png") || !url.ends_with(".jpg") || !url.ends_with(".gif") {
+        } else if !url.ends_with(".png") && !url.ends_with(".jpg") && !url.ends_with(".gif") {
             return Some(format!("{}.png", url));
         } else {
             return Some(url.to_owned());
@@ -94,56 +102,395 @@ fn is_image(url: &str) -> Option<String> {
     None
 }
 
+async fn get_distance(
+    cache_actor: &Sender<DistanceCacheMessage>,
+    dist_actor: &Sender<TestColorMessage>,
+    url: &String,
+    desired_color: Lab,
+) -> u32 {
+    let (w, s) = oneshot::channel();
+    cache_actor
+        .send(DistanceCacheMessage::Read(url.clone(), w))
+        .await
+        .unwrap();
+    if let Some(lab) = s.await.unwrap() {
+        return lab_distance(&lab, &desired_color) as u32;
+    }
+
+    let (w, s) = oneshot::channel();
+    if let Err(_) = dist_actor
+        .send(TestColorMessage(url.clone(), desired_color, w))
+        .await
+    {
+        return u32::MAX;
+    }
+    match s.await {
+        Err(_) => u32::MAX,
+        Ok(None) => u32::MAX,
+        Ok(Some((dominant_color, distance))) => {
+            let _ = cache_actor
+                .send(DistanceCacheMessage::Write(url.clone(), dominant_color))
+                .await;
+            distance
+        }
+    }
+}
+
+fn b(x: i32) {}
+fn get_reddit_with_progress(
+    q: String,
+    lab: Lab,
+    cache_actor: Sender<DistanceCacheMessage>,
+    dist_actor: Sender<TestColorMessage>,
+) -> Receiver<String> {
+    let (progress, r) = async_channel::unbounded::<String>();
+    let reddit = get_reddit(q, lab, cache_actor, dist_actor, progress);
+    tokio::spawn(reddit);
+    info!("after spwan");
+    r
+}
 //https://www.reddit.com/r/php/search.json?q=oop&limit=5&sort=hot&restrict_sr=0
-fn get_reddit(q: String) -> SearchResult {
+async fn get_reddit(
+    q: String,
+    lab: Lab,
+    cache_actor: Sender<DistanceCacheMessage>,
+    dist_actor: Sender<TestColorMessage>,
+    progress: Sender<String>,
+) -> SearchResult {
+    progress.send("{\"v\":0}".to_owned()).await;
+
     let mut images: Vec<RedditResultDataChildrenData> = Vec::new();
-    let qtd = 3;
+    let mut qtd = 3;
+    let mut currenti = 0.0f32;
+    let total = qtd * 100;
     let mut after = None;
 
+    use std::collections::BinaryHeap;
+    let mut candidates = BinaryHeap::new();
+
+    let reddit_search_limit = 1000;
     let cont = true;
     while cont {
         let url = match after {
-            None => format!("https://www.reddit.com/r/php/search.json?q={}%20site:(500px.com%20OR%20abload.de%20OR%20deviantart.com%20OR%20deviantart.net%20OR%20fav.me%20OR%20fbcdn.net%20OR%20flickr.com%20OR%20forgifs.com%20OR%20giphy.com%20OR%20gfycat.com%20OR%20gifsoup.com%20OR%20gyazo.com%20OR%20imageshack.us%20OR%20imgclean.com%20OR%20imgur.com%20OR%20instagr.am%20OR%20instagram.com%20OR%20mediacru.sh%20OR%20media.tumblr.com%20OR%20min.us%20OR%20minus.com%20OR%20myimghost.com%20OR%20photobucket.com%20OR%20picsarus.com%20OR%20puu.sh%20OR%20staticflickr.com%20OR%20tinypic.com%20OR%20twitpic.com)&limit=5&sort=comments&restrict_sr=0", q),
-            Some(after) => format!("https://www.reddit.com/r/php/search.json?q={}%20site:(500px.com%20OR%20abload.de%20OR%20deviantart.com%20OR%20deviantart.net%20OR%20fav.me%20OR%20fbcdn.net%20OR%20flickr.com%20OR%20forgifs.com%20OR%20giphy.com%20OR%20gfycat.com%20OR%20gifsoup.com%20OR%20gyazo.com%20OR%20imageshack.us%20OR%20imgclean.com%20OR%20imgur.com%20OR%20instagr.am%20OR%20instagram.com%20OR%20mediacru.sh%20OR%20media.tumblr.com%20OR%20min.us%20OR%20minus.com%20OR%20myimghost.com%20OR%20photobucket.com%20OR%20picsarus.com%20OR%20puu.sh%20OR%20staticflickr.com%20OR%20tinypic.com%20OR%20twitpic.com)&limit=5&sort=comments&restrict_sr=0&after={}", 
-                q, &after)
+            None => format!("https://www.reddit.com/r/php/search.json?q={}%20site:(500px.com%20OR%20abload.de%20OR%20deviantart.com%20OR%20deviantart.net%20OR%20fav.me%20OR%20fbcdn.net%20OR%20flickr.com%20OR%20forgifs.com%20OR%20giphy.com%20OR%20gfycat.com%20OR%20gifsoup.com%20OR%20gyazo.com%20OR%20imageshack.us%20OR%20imgclean.com%20OR%20imgur.com%20OR%20instagr.am%20OR%20instagram.com%20OR%20mediacru.sh%20OR%20media.tumblr.com%20OR%20min.us%20OR%20minus.com%20OR%20myimghost.com%20OR%20photobucket.com%20OR%20picsarus.com%20OR%20puu.sh%20OR%20staticflickr.com%20OR%20tinypic.com%20OR%20twitpic.com)&limit={}&sort=comments&restrict_sr=0", q, reddit_search_limit),
+            Some(after) => format!("https://www.reddit.com/r/php/search.json?q={}%20site:(500px.com%20OR%20abload.de%20OR%20deviantart.com%20OR%20deviantart.net%20OR%20fav.me%20OR%20fbcdn.net%20OR%20flickr.com%20OR%20forgifs.com%20OR%20giphy.com%20OR%20gfycat.com%20OR%20gifsoup.com%20OR%20gyazo.com%20OR%20imageshack.us%20OR%20imgclean.com%20OR%20imgur.com%20OR%20instagr.am%20OR%20instagram.com%20OR%20mediacru.sh%20OR%20media.tumblr.com%20OR%20min.us%20OR%20minus.com%20OR%20myimghost.com%20OR%20photobucket.com%20OR%20picsarus.com%20OR%20puu.sh%20OR%20staticflickr.com%20OR%20tinypic.com%20OR%20twitpic.com)&limit={}&sort=comments&restrict_sr=0&after={}", 
+                q, 
+                &after,
+                reddit_search_limit)
         };
         // info!("Reddit URL: {}", url);
-        let mut response = Request::get(url)
+        let mut response = Request::get(&url)
             .header("User-Agent", "linux:isahc:searchapi")
             .body(())
             .unwrap()
-            .send()
+            .send_async()
+            .await
             .unwrap();
-        let r = response.json::<RedditResult>().unwrap();
-        after = Some(r.data.after);
-        for i in r.data.children.iter() {
+        let reddit = response.json::<RedditResult>();
+        if reddit.is_err() {
+            info!("{:?} {}", reddit, url);
+            break;
+        }
+        let reddit = reddit.unwrap();
+        after = Some(reddit.data.after);
+        for i in reddit.data.children.iter() {
+            currenti += 1.0;
             let data = &i.data;
             if let Some(url) = is_image(&data.url) {
-                let mut data = i.data.clone();
-                data.url = url;
-                images.push(data);
+                info!("{}", currenti / total as f32);
+                progress.send(format!("{{\"v\":{},\"msg\":\"{}\"}}",
+                    currenti / total as f32,
+                    url)
+                ).await;
+                let distance = get_distance(&cache_actor, &dist_actor, &url, lab).await;
+                let mut data = data.clone();
+                data.url = url.clone();
+                candidates.push(Reverse(OrdFirst(distance, data)));
             }
 
-            if images.len() == qtd {
+            if candidates.len() == total {
                 break;
             }
         }
 
-        if images.len() == qtd {
+        if candidates.len() == total {
             break;
         }
     }
 
-    SearchResult { images }
+    info!("{} {}", candidates.len(), total);
+    debug!("{:?}", candidates);
+
+    while qtd > 0 {
+        qtd -= 1;
+        match candidates.pop() {
+            None => break,
+            Some(data) => {
+                images.push((data.0).1);
+            }
+        }
+    }
+
+    progress.send("{\"v\":100}".to_owned()).await;
+    let r = SearchResult { images };
+    let r_json = serde_json::to_string(&r).unwrap();
+    progress.send(r_json).await;
+    r
 }
 
-#[get("/search?<q>")]
-fn search(q: String) -> Json<SearchResult> {
-    trace!("search-api: q={}", q);
-    Json(get_reddit(q))
+#[derive(Debug)]
+struct OrdFirst<TA, TB>(TA, TB);
+
+impl<TA: Ord, TB> Ord for OrdFirst<TA, TB> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.0.cmp(&other.0)
+    }
 }
 
-fn main() {
+impl<TA: PartialOrd, TB> PartialOrd for OrdFirst<TA, TB> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl<TA: Eq, TB> Eq for OrdFirst<TA, TB> {}
+
+impl<TA: PartialEq, TB> PartialEq for OrdFirst<TA, TB> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+#[derive(Deserialize)]
+struct SearchQueryString {
+    q: Option<String>,
+    r: Option<f32>,
+    g: Option<f32>,
+    b: Option<f32>,
+}
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum ErrorCode {
+    #[error("invalid rdo_lookahead_frames {0} (expected < {})", i32::max_value())]
+    Error,
+}
+
+async fn search(
+    q: SearchQueryString,
+    cache: Sender<DistanceCacheMessage>,
+    distance: Sender<TestColorMessage>,
+) -> Result<impl warp::reply::Reply, Infallible> {
+    info!("Search");
+    use futures::StreamExt;
+    let str_to_sse_data = |x| match Some(x) {
+        Some(x) => Ok(warp::sse::data(x)),
+        None => Err(ErrorCode::Error),
+    };
+    match (q.q, q.r, q.g, q.b) {
+        (Some(q), Some(r), Some(g), Some(b)) => {
+            let lab = palette::rgb::Srgb::new(r, g, b).into_lab();
+            let progress = get_reddit_with_progress(q, lab, cache, distance);
+            let progress = progress.map(str_to_sse_data);
+            Ok(warp::sse::reply(progress))
+        }
+        _ => {
+            let (w, progress) = async_channel::unbounded::<String>();
+            w.send("Error".to_owned()).await;
+            let progress = progress.map(str_to_sse_data);
+            Ok(warp::sse::reply(progress))
+        }
+    }
+}
+
+use kmeans_colors::{get_kmeans, Calculate, Kmeans, MapColor, Sort};
+use palette::IntoColor;
+use palette::{Lab, Pixel, Srgb};
+
+fn lab_distance(a: &Lab, b: &Lab) -> f32 {
+    let x = a.l - b.l;
+    let y = a.a - b.a;
+    let z = a.b - b.b;
+    (x * x + y * y + z * z).sqrt()
+}
+
+fn get_image_pixels(data: &[u8]) -> Result<Vec<Lab>, i32> {
+    let img = image::load_from_memory(&data).or(Err(0))?;
+    let img = img.resize(32, 32, image::imageops::FilterType::Nearest);
+    let rgb8 = img.to_rgb();
+    let pixels = rgb8.into_raw();
+    Ok(Srgb::from_raw_slice(&pixels)
+        .iter()
+        .map(|x| x.into_format().into())
+        .collect())
+}
+
+fn get_dominant_color(pixels: &Vec<Lab>) -> Lab {
+    let runs = 1;
+    let k = 3;
+    let max_iter = 1;
+    let converge = 0.1;
+    let verbose = false;
+    let seed = 0;
+    let mut result = Kmeans::new();
+    (0..runs).for_each(|i| {
+        let run_result = get_kmeans(k, max_iter, converge, verbose, &pixels, seed + i as u64);
+        if run_result.score < result.score {
+            result = run_result;
+        }
+    });
+    // let rgb = &result
+    //     .centroids
+    //     .iter()
+    //     .map(|x| Srgb::from(*x).into_format())
+    //     .collect::<Vec<Srgb<u8>>>();
+    // let buffer = Srgb::map_indices_to_centroids(&rgb, &result.indices);
+    let mut res = Lab::sort_indexed_colors(&result.centroids, &result.indices);
+    // let dominant_color = Lab::get_dominant_color(&res);
+    res.sort_unstable_by(|a, b| (b.percentage).partial_cmp(&a.percentage).unwrap());
+    res.first().unwrap().centroid
+}
+
+async fn handle(msg: TestColorMessage) {
+    let url = msg.0;
+    let desired_color = msg.1;
+    let reply = msg.2;
+
+    let mut url = url;
+    let mut tries = 10u8;
+    let response = loop {
+        if tries <= 0 {
+            break None;
+        }
+
+        let response = Request::get(url)
+            .body(())
+            .unwrap()
+            .send_async()
+            .await
+            .unwrap();
+
+        let status = response.status().as_u16();
+        if status == 200 {
+            break Some(response);
+        } else if status == 301 {
+            let headers = response.headers();
+            let location = headers.get("Location").unwrap();
+            url = location.to_str().unwrap().to_owned();
+            debug!(target: "test_color_actor", "redirected to: {}", url);
+            tries -= 1;
+        } else {
+            break None;
+        }
+    };
+
+    match response {
+        None => {
+            reply.send(None).unwrap();
+        }
+        Some(mut response) => {
+            let mut img_data = Vec::new();
+            response.copy_to(&mut img_data).unwrap();
+            let pixels = match get_image_pixels(&img_data) {
+                Err(_) => {
+                    reply.send(None).unwrap();
+                    return;
+                }
+                Ok(pixels) => pixels,
+            };
+            let dominant_color = get_dominant_color(&pixels);
+            let distance = lab_distance(&desired_color, &dominant_color);
+            reply.send(Some((dominant_color, distance as u32)));
+
+            debug!(target: "test_color_actor", "distance: {}", distance);
+        }
+    }
+}
+
+struct TestColorMessage(String, Lab, oneshot::Sender<Option<(Lab, u32)>>);
+async fn test_color_actor(r: async_channel::Receiver<TestColorMessage>) {
+    loop {
+        match r.recv().await {
+            Ok(msg) => handle(msg).await,
+            Err(_) => {}
+        }
+    }
+}
+
+enum DistanceCacheMessage {
+    Write(String, Lab),
+    Read(String, oneshot::Sender<Option<Lab>>),
+}
+async fn distance_cache(r: async_channel::Receiver<DistanceCacheMessage>) {
+    loop {
+        match r.recv().await {
+            Ok(DistanceCacheMessage::Write(url, lab)) => {
+                std::fs::create_dir(".cache");
+
+                let digest = md5::compute(&url);
+                let path = format!(".cache/{:x}.txt", digest);
+                let mut f = std::fs::File::create(path).unwrap();
+                use std::io::prelude::*;
+                f.write_all(format!("{}\n", lab.l).as_bytes()).unwrap();
+                f.write_all(format!("{}\n", lab.a).as_bytes()).unwrap();
+                f.write_all(format!("{}\n", lab.b).as_bytes()).unwrap();
+                debug!(target: "distance_cache", "cache written: {:x} {}", digest, url);
+            }
+            Ok(DistanceCacheMessage::Read(url, reply)) => {
+                std::fs::create_dir(".cache");
+                let digest = md5::compute(&url);
+                let path = format!(".cache/{:x}.txt", digest);
+                let distance = match std::fs::File::open(path) {
+                    Err(_) => {
+                        debug!(target: "distance_cache", "not found: {:x} {}", digest, url);
+                        None
+                    }
+                    Ok(mut f) => {
+                        debug!(target: "distance_cache", "found: {:x} {}", digest, url);
+                        let mut txt = String::with_capacity(100);
+                        use std::io::prelude::*;
+                        f.read_to_string(&mut txt);
+                        let parts: Vec<&str> = txt.split("\n").collect();
+                        Some(Lab::from_components((
+                            parts[0].parse::<f32>().unwrap(),
+                            parts[1].parse::<f32>().unwrap(),
+                            parts[2].parse::<f32>().unwrap(),
+                        )))
+                    }
+                };
+                reply.send(distance);
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
-    rocket::ignite().mount("/", routes![search]).launch();
+
+    let distance_cache_actor;
+    let search_actor;
+
+    {
+        let (w, r) = async_channel::unbounded::<DistanceCacheMessage>();
+        distance_cache_actor = warp::any().map(move || w.clone());
+        tokio::spawn(distance_cache(r));
+    }
+    {
+        let (w, r) = async_channel::unbounded::<TestColorMessage>();
+        search_actor = warp::any().map(move || w.clone());
+        tokio::spawn(test_color_actor(r));
+    }
+
+    let search_endpoint = warp::get()
+        .and(warp::path("search"))
+        .and(warp::query::<SearchQueryString>())
+        .and(distance_cache_actor.clone())
+        .and(search_actor.clone())
+        .and_then(search);
+    warp::serve(search_endpoint)
+        .run(([127, 0, 0, 1], 8000))
+        .await
 }
--- frontend/wwwroot/index.jsx
+++ frontend/wwwroot/index.jsx
@@ -1,11 +1,60 @@
 import { h, hydrate, Fragment } from 'preact';
-import { useEffect, useRef } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import flickity from 'flickity';
+import VanillaPicker from 'vanilla-picker';
 import useInput from "./useInput.js";
 import useAsync from "./useAsync.js";
+import './loading-bar/loading-bar';
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function useColorPicker(initialColor, onChange) {
+    const ref = useRef();
+    const control = useRef();
+    const [color, set] = useState(initialColor);
+    console.log(2);
+    useEffect(() => {
+        if (!control.current) {
+            control.current = new VanillaPicker({
+                parent: ref.current,
+                color,
+                alpha: false
+            });
+            control.current.onChange = function (color) {
+                set(color.rgba);
+            };
+        }
+    }, [ref.current]);
+
+    return [ref, color];
+}
+
+function usePrevious(state) {
+    let ref = useRef(state);
+    let tmp = ref.current;
+    ref.current = state;
+    return tmp;
+}
+
+function useProgressBar() {
+    const ref = useRef();
+    const previousRef = usePrevious(ref);
+    const barValue = useRef();
+    useEffect(() => {
+        if (ref.current) {
+            barValue.current = new ldBar(ref.current, {
+                path: "M213.149867,129.220267 C213.149867,118.843733 204.758756,110.603378 194.532978,110.603378 C189.498311,110.603378 184.918756,112.585956 181.562311,115.791644 C168.745244,106.635378 151.195022,100.6848 131.662222,99.9224889 L140.206933,59.9409778 L167.980089,65.8915556 C168.287289,72.9116444 174.084267,78.5578667 181.257956,78.5578667 C188.5824,78.5578667 194.532978,72.6072889 194.532978,65.28 C194.532978,57.9555556 188.5824,52.0049778 181.257956,52.0049778 C176.069689,52.0049778 171.490133,55.0570667 169.353956,59.4830222 L138.377956,52.9208889 C137.462044,52.7672889 136.546133,52.9208889 135.934578,53.3788444 C135.172267,53.8368 134.714311,54.5991111 134.563556,55.5150222 L125.100089,100.073244 C105.262933,100.6848 87.4083556,106.635378 74.4376889,115.945244 C71.0812444,112.739556 66.5016889,110.756978 61.4670222,110.756978 C51.0904889,110.756978 42.8501333,119.148089 42.8501333,129.373867 C42.8501333,137.002667 47.4268444,143.4112 53.8382222,146.312533 C53.5310222,148.141511 53.3802667,149.973333 53.3802667,151.958756 C53.3802667,180.644978 86.7996444,203.995022 128.001422,203.995022 C169.2032,203.995022 202.622578,180.798578 202.622578,151.958756 C202.622578,150.126933 202.468978,148.141511 202.164622,146.312533 C208.573156,143.4112 213.149867,136.849067 213.149867,129.220267 Z M85.2721778,142.495289 C85.2721778,135.170844 91.2227556,129.220267 98.5500444,129.220267 C105.874489,129.220267 111.825067,135.170844 111.825067,142.495289 C111.825067,149.819733 105.874489,155.773156 98.5500444,155.773156 C91.2227556,155.923911 85.2721778,149.819733 85.2721778,142.495289 Z M159.588978,177.746489 C150.432711,186.902756 133.036089,187.514311 128.001422,187.514311 C122.813156,187.514311 105.416533,186.749156 96.4110222,177.746489 C95.04,176.372622 95.04,174.236444 96.4110222,172.862578 C97.7848889,171.491556 99.9210667,171.491556 101.294933,172.862578 C107.094756,178.6624 119.303111,180.644978 128.001422,180.644978 C136.699733,180.644978 149.058844,178.6624 154.705067,172.862578 C156.078933,171.491556 158.215111,171.491556 159.588978,172.862578 C160.809244,174.236444 160.809244,176.372622 159.588978,177.746489 Z M157.1456,155.923911 C149.821156,155.923911 143.870578,149.973333 143.870578,142.648889 C143.870578,135.324444 149.821156,129.373867 157.1456,129.373867 C164.472889,129.373867 170.423467,135.324444 170.423467,142.648889 C170.423467,149.819733 164.472889,155.923911 157.1456,155.923911 Z",
+                fill: "data:ldbar/res,bubble(#FF4500,#fff,50,1)",
+                type: "fill",
+            });
+        }
+    });
+    return [ref, (x) => {
+        if (barValue.current)
+            barValue.current.set(x * 100);
+    }];
 }
 
 function Flickity({ children }) {
@@ -21,26 +70,90 @@ function Flickity({ children }) {
     </div>;
 }
 
+function getActiveElement() {
+    return window.document.activeElement
+}
+
 function App() {
-    const searchInput = useInput("");
-    const [
+    const [colorRef, [color_r, color_g, color_b]] = useColorPicker([0, 0, 0, 0]);
+    const [progressRef, setProgress] = useProgressBar();
+    const searchInput = useInput("ferrari");
+    const logRef = useRef();
+    let [
         errorSearch,
         executeSearch,
         pendingSearch,
         valueSearch,
     ] = useAsync(async () => {
+        console.log("searching...");
         const q = searchInput.value;
-        const res = await fetch(`/search?q=${q}`);
-        const json = await res.json();
-        return json;
+        const url = `/search?q=${q}&r=${color_r / 255.0}&g=${color_g / 255.0}&b=${color_b / 255.0}`;
+        return new Promise((ok, rej) => {
+            var source = new EventSource(url);
+            source.onmessage = (e) => {
+                const data = JSON.parse(e.data);
+                if (data.v) {
+                    setProgress(data.v);
+                    if (logRef.current) {
+                        logRef.current.innerText = data.msg;
+                        logRef.current.animate([
+                            { opacity: 0, transform: "translateX(900%)", offset: 0 },
+                            { opacity: 1, transform: "translateX(0%)", offset: 0.3 },
+                            { opacity: 1, transform: "translateX(0%)", offset: 0.7 },
+                            { opacity: 0, transform: "translateX(-900%)", offset: 1 }
+                        ], {
+                            duration: 1000,
+                            iterations: 1,
+                        })
+                    }
+                }
+                else if (data.images) {
+                    source.close();
+                    ok(data);
+                }
+            };
+        })
     }, false);
-    console.log(valueSearch);
+    useEffect(() => {
+        document.body.style.background = `rgba(${color_r},${color_g},${color_b},1)`;
+    });
+    let textColor = color_r * 0.2126 + color_g * 0.7152 + color_b * 0.0722;
+    if (textColor > 128) textColor = 0;
+    else textColor = 255;
+    let cssColor = `color:rgba(${textColor},${textColor},${textColor},255)`;
+    let cssColorBorderBottom = `border-bottom-color: rgba(${textColor},${textColor},${textColor},255)`;
+
+    const inputRef = useRef();
+    const open = useRef();
     return <>
-        <label>Subject:</label>
-        <input {...searchInput} />
-        <button onClick={executeSearch}>Search</button>
-        <div>
-            {pendingSearch && <span>Loading...</span>}
+        <form action="" autocomplete="on" onSubmit={(e) => {
+            executeSearch();
+            return false;
+        }} class={pendingSearch ? "is-blurred" : ""}>
+            <div ref={colorRef} style="display:inline-block">
+                <i class="fa fa-paint-brush" aria-hidden="true" style={cssColor}></i>
+            </div>
+            <input ref={inputRef} type="text" style={`${cssColor};${cssColorBorderBottom}`} placeholder="What're we looking for ?" {...searchInput} />
+            <div id="button" onClick={() => {
+                if (!open.current) {
+                    inputRef.current.focus();
+                    open.current = true;
+                } else {
+                    inputRef.current.blur();
+                    open.current = false;
+                    executeSearch();
+                }
+            }} >
+                <i class="fa fa-search" aria-hidden="true" style={cssColor}></i>
+            </div>
+
+        </form>
+        <div style="padding-top:50px">
+            {pendingSearch &&
+                <div class="Modal">
+                    <div class="Modal-content" ref={progressRef} />
+                    <div ref={logRef} style={"padding-top:10px;text-align:center;" + cssColor}>Let's see what we find...</div>
+                </div>}
             {valueSearch && <Flickity>
                 {valueSearch.images
                     .map(x => <div class="carousel-cell"><img src={x.url} style="width:100%" /></div>)}
